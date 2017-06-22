98108e03d62e4ff240d5ab2daf7a482b6e020c1a
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-22 15:21:35 +0530

    Added post loop with custom classes

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php get_header(); ?>
 
-<h1>This is first page</h1>
+    <?php
+        if( is_front_page()) {
+            $sgcratckit_classes = array('container');
+        }
+        else {
+            $sgcratckit_classes = array('card');
+        }
+    ?>
+
+    <?php if( have_posts() ) {
+        
+        while( have_posts() ) {
+            the_post(); ?>
+            <div <?php body_class($sgcratckit_classes); ?>>
+                <h3><?php the_title(); ?></h3>
+                <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+                <p><?php the_content(); ?></p>
+            </div>
+            <hr>
+        <?php }
+    
+    }
+    ?>
 
 <?php get_footer(); ?>

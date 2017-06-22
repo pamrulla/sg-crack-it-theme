0d3bb1fa28abc0bccbd84c87fc679a3dc779b264
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-22 14:52:42 +0530

    Added basic wp menu system

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
             <h1>This is footer</h1>
         </footer>
         
+        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
         <?php wp_footer(); ?>
     </body>
 </html>

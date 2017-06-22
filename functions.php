9ef6ba3b5915593ab1e19658c94ba6c125de2d13
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-22 14:26:43 +0530

    Loading css and javascript using functions.ph[

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function sgcrackit_script_enqueue() {
+    
+    wp_enqueue_style('bootstrapstyle', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css', array());
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/sgcrackit.css', array(), '1.0.0', 'all');
+    
+    wp_enqueue_script('boostrapscript', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', array('jquery'), null, true);
+    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/sgcrackit.js', array(), '1.0.0', true);
+    
+}
+
+add_action('wp_enqueue_scripts', 'sgcrackit_script_enqueue');

e799414753ab7fc4a2459a935562a8e110e5f01c
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-27 15:31:35 +0530

    Create taxonomy-language.php

---
+++ taxonomy-language.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div id="content" role="main">
+                <?php get_template_part('loops/content', 'language'); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>

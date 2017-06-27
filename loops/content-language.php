608fb1426e5b80670589e3970b6e00fbe5436b59
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-27 15:32:54 +0530

    Create content-language.php

---
+++ loops/content-language.php
@@ -0,0 +1,22 @@
+<?php
+/*
+The Single Posts Loop
+=====================
+*/
+?> 
+
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
+        <header>
+            <h2><?php the_title()?></h2>
+        </header>
+        <section>
+            <?php the_post_thumbnail(); ?>
+            <?php the_content()?>
+            <?php the_permalink(); ?>
+        </section>
+    </article>
+<?php endwhile; ?>
+<?php else: ?>
+<?php wp_redirect(get_bloginfo('url').'/404', 404); exit; ?>
+<?php endif; ?>

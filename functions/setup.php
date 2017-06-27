284f2c096627f8d3727489646f4cb4353486769b
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-27 16:20:38 +0530

    Added generation of home page and all languages page

--- functions/setup.php
+++ functions/setup.php
@@ -19,3 +19,60 @@ add_filter('excerpt_more', 'sgcrackit_excerpt_readmore');
 
 // Add post formats support. See http://codex.wordpress.org/Post_Formats
 add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+
+// setup a function to check if these pages exist
+function the_slug_exists($post_name) {
+	global $wpdb;
+	if($wpdb->get_row("SELECT post_name FROM wp_posts WHERE post_name = '" . $post_name . "'", 'ARRAY_A')) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// change the Sample page to the home page
+if (isset($_GET['activated']) && is_admin()){
+    $home_page_title = 'Home';
+    $home_page_content = '';
+    $home_page_check = get_page_by_title($home_page_title);
+    $home_page = array(
+	    'post_type' => 'page',
+	    'post_title' => $home_page_title,
+	    'post_content' => $home_page_content,
+	    'post_status' => 'publish',
+	    'post_author' => 1,
+	    'post_slug' => 'home'
+    );
+    if(!isset($home_page_check->ID) && !the_slug_exists('home')){
+        $home_page_id = wp_insert_post($home_page);
+    }
+}
+
+// change the Sample page to the home page
+if (isset($_GET['activated']) && is_admin()){
+    $home_page_title = 'All Languages';
+    $home_page_content = '';
+    $home_page_check = get_page_by_title($home_page_title);
+    $home_page = array(
+	    'post_type' => 'page',
+	    'post_title' => $home_page_title,
+	    'post_content' => $home_page_content,
+	    'post_status' => 'publish',
+	    'post_author' => 1,
+	    'post_slug' => 'all-languages',
+        'meta_input' => array(
+            '_wp_page_template' => 'page-all-languages.php'
+        )
+    );
+    if(!isset($home_page_check->ID) && !the_slug_exists('all-languages')){
+        $home_page_id = wp_insert_post($home_page);
+    }
+}
+
+$homepage = get_page_by_title( 'Home' );
+
+if ( $homepage )
+{
+    update_option( 'page_on_front', $homepage->ID );
+    update_option( 'show_on_front', 'page' );
+}

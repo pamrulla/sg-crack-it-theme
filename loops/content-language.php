cdf753e82f363f6fceeec0f98fe009565ccb89de
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-28 16:34:44 +0530

    Update content-language.php

--- loops/content-language.php
+++ loops/content-language.php
@@ -3,20 +3,46 @@
 The Single Posts Loop
 =====================
 */
+
+$sgQuizes = array();
 ?> 
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
-    <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-        <header>
-            <h2><?php the_title()?></h2>
-        </header>
-        <section>
-            <?php the_post_thumbnail(); ?>
-            <?php the_content()?>
-            <?php the_permalink(); ?>
-        </section>
-    </article>
+        <?php 
+            $sgterm = get_the_terms(the_ID(), 'level'); 
+            if(!is_wp_error($term))
+            {
+                if($sgterm[0]->name == "Beginner"){
+                    $sgQuizes[0] = $post;
+                }
+                else if($sgterm[0]->name == "Intermediate"){
+                    $sgQuizes[1] = $post;
+                }
+                else if($sgterm[0]->name == "Advanced"){
+                    $sgQuizes[2] = $post;
+                }
+            }
+        ?>
 <?php endwhile; ?>
+
+<div class="card-deck">
+    <?php for($i = 0; $i < count($sgQuizes); $i++)
+        { 
+    ?>
+            <div class="card <?php if($i == 0) { echo 'card-outline-danger'; } else if($i == 1) { echo 'card-outline-warning'; } else if($i == 2) { echo 'card-outline-success'; } ?> ">
+                <div class="card-block">
+                    <h4 class="card-title <?php if($i == 0) { echo 'text-danger'; } else if($i == 1) { echo 'text-warning'; } else if($i == 2) { echo 'text-success'; } ?>"><?php echo $sgQuizes[$i]->post_title; ?></h4>
+                    <p class="card-text"><?php echo $sgQuizes[$i]->post_content; ?></p>
+                </div>
+                <div class="card-footer text-right">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <a href="<?php echo get_permalink($sgQuizes[$i]->ID) ?>" class="btn <?php if($i == 0) { echo 'btn-danger'; } else if($i == 1) { echo 'btn-warning'; } else if($i == 2) { echo 'btn-success'; } ?>">Start</a>
+                </div>
+            </div>
+    <?php
+        }
+    ?>
+</div>
 <?php else: ?>
 <?php wp_redirect(get_bloginfo('url').'/404', 404); exit; ?>
 <?php endif; ?>

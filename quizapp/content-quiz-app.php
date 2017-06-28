8cf3976d99a6ad03bfafeb462ec0996da18ca823
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-28 17:30:19 +0530

    Update content-quiz-app.php

--- quizapp/content-quiz-app.php
+++ quizapp/content-quiz-app.php
@@ -1,4 +1,52 @@
-<h1>Khan</h1>
+<div class="card">
+    <div class="card-header">
+        <h4>This is title</h4>
+    </div>
+    <div class="card-block">
+        <div class="quiz-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 25%; height: 3px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <div class="row">
+                    <div class="col-sm-10">
+                        <h5>This is the question</h5>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="countdown">
+                            <div class="countdown-number"></div>
+                            <svg>
+                                <circle r="18" cx="20" cy="20"></circle>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer text-center">
+        <small class="text-muted">This is presented by <a href="www.smartgnan.com" target="_blank">www.smartgnan.com</a></small>
+    </div>
+</div>
+
+<script>
+    var countdownNumberEl = document.getElementsByClassName('countdown-number')[0];
+    var countdown = 30;
+
+    countdownNumberEl.textContent = countdown+'s';
+
+    setInterval(function() {
+      countdown = --countdown < 0 ? 30 : countdown;
+
+      countdownNumberEl.textContent = countdown+'s';
+    }, 1000);
+</script>
+
 <div class="custom-controls-stacked">
     <label class="custom-control custom-checkbox">
       <input type="checkbox" class="custom-control-input" id="check1" name="check-1">

59d3cda69a75d032f2bb5ae8e2d0dc97fd761e44
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-30 17:05:49 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,3 +5,4 @@ require get_template_directory() . '/functions/posttypes.php';
 require get_template_directory() . '/functions/setup.php';
 require get_template_directory() . '/functions/enqueues.php';
 require get_template_directory() . '/functions/navbar.php';
+require get_template_directory() . '/functions/ajaxhandler.php';
